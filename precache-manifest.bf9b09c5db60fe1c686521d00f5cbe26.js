a2add301153e39bb80c9484df2b33a22e6b10023
Praveen <apple@Apples-MacBook-Air.local>
2019-05-04 12:09:05 +0530

    initial

---
+++ precache-manifest.bf9b09c5db60fe1c686521d00f5cbe26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625ad98d8dee5f7f29a54b8e08400d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "833521372da9830d67bd",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "247a7a59f3d73dd69701",
+    "url": "/static/css/main.0ea3b9cd.chunk.css"
+  },
+  {
+    "revision": "833521372da9830d67bd",
+    "url": "/static/js/2.a55ac973.chunk.js"
+  },
+  {
+    "revision": "247a7a59f3d73dd69701",
+    "url": "/static/js/main.7f98447e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
